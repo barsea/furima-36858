0df6cc9b1231595dd3dac14ad910dff0c2b8f6d2
barsea <shiba21t@gmail.com>
2022-02-01 23:01:28 +0900

    カテゴリー選択などにActiveHashを実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :delivery_charge, :prefecture, :days_to_ship
+
   belongs_to :user
   has_one_attached :image
   # has_one :order # 後で
@@ -10,5 +13,5 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }, numericality: {only_integer: true, message: "is integer only" }
 end
