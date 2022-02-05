12a81c01a36cf87c82c585f63d27c2a8ff40c2e6
barsea <shiba21t@gmail.com>
2022-02-05 20:16:17 +0900

    購入ページへの遷移制限、購入ページの商品情報表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,18 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: :index
+  before_action :move_to_index, only: :index
+
   def index
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+      redirect_to root_path if current_user == @item.user
   end
 end
