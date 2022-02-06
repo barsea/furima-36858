edae80d497106dd2d878d6852796a049c980cf4e
barsea <shiba21t@gmail.com>
2022-02-07 03:10:52 +0900

    クレジットカード決済機能実装、テストコード追記

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :item_id
@@ -9,6 +9,7 @@ class OrderAddress
     validates :city
     validates :house_number
     validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number' }, length: { in: 10..11, message: 'is too short' }
+    validates :token
   end
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
